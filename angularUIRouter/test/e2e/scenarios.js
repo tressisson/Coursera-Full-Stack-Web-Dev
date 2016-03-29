64322764fc640eefd0bc5ecc0835163d5979ca50
William Sisson <wsisson@ENG-MACBOOK-03.local>
2016-03-29 09:28:00 -0500

    finished e2e testing assignment

--- angularUIRouter/test/e2e/scenarios.js
+++ angularUIRouter/test/e2e/scenarios.js
@@ -21,3 +21,32 @@ describe('conFusion App E2E Testing', function() {
     });
 
 });
+
+describe('menu 0 item', function() {
+    beforeEach(function() {
+        browser.get('#/menu/0');
+    });
+
+    it('should have a name', function() {
+        var name = element(by.binding('dish.name'));
+        expect(name.getText()).
+            toEqual('Uthapizza Hot $4.99');
+    });
+
+    it('should show the number of comments as', function() {
+        expect(element.all(by.repeater('entry in dish.comments'))
+            .count()).toEqual(5);
+
+    });
+
+    it('should show the first comment author as', function() {
+        element(by.model('sortComments')).sendKeys('author');
+        expect(element.all(by.repeater('entry in dish.comments'))
+            .count()).toEqual(5);
+        var author = element.all(by.repeater('entry in dish.comments'))
+            .first().element(by.binding('entry.author'));
+
+        expect(author.getText()).toContain('25 Cent');
+
+    });
+});  
