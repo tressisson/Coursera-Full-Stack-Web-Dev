b7b13c9a4fdf2b2bc8bc32ec89c253deb2e82fb9
unknown <wsisson@Eng-WS-15.ESTECH.com>
2016-03-29 08:54:32 -0500

    working on protractor assignment

---
+++ angularUIRouter/test/e2e/scenarios.js
@@ -0,0 +1,23 @@
+'use strict';
+
+describe('conFusion App E2E Testing', function() {
+
+    it('should automatically redirect to / when location hash/fragment is empty', function() {
+
+        browser.get('index.html');
+        expect(browser.getLocationAbsUrl()).toMatch("/");
+
+        describe('index', function() {
+            beforeEach(function() {
+                browser.get('index.html#/');
+            });
+
+            it('should have a title', function() {
+                expect(browser.getTitle()).
+                    toEqual('Ristorante Con Fusion');
+            });
+        });
+
+    });
+
+});
